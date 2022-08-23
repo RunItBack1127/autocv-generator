b369e653fcb57c854a000d9149249a3824bffc09
Wes <thewikiscience@gmail.com>
2022-08-23 01:21:58 -0400

    Templating component classes

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'SiteHeader'
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
