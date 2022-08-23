672e8c3f84e6dc7d561300f8435fa8cb1d4b3b77
Wes <thewikiscience@gmail.com>
2022-08-23 01:20:07 -0400

    Initial implementation of site layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <template>
-
+  <main class="site-container">
+    <SiteHeader />
+    <section class="form-container">
+      <PDFForm />
+    </section>
+  </main>
 </template>
 
 <script setup lang="ts">
+import { defineComponent } from 'vue';
 
+import SiteHeader from '@/components/SiteHeader.vue';
+import PDFForm from '@/components/PDFForm.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    SiteHeader,
+    PDFForm
+  }
+});
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;900&display=swap');
 
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Roboto, sans-serif;
+  font-size: 16px;
+  background: #fff;
+  color: #000;
+}
 </style>
